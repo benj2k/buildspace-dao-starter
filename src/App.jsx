7a4fbbd0ff9689ce14c490895ebf3017cd74b04d
Benjamin Darthout <benjamin.darthout@gmail.com>
2022-06-02 18:07:02 +0200

    Allow connect to wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
 const App = () => {
+
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("address : ", address);
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to CopyPasteDAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Wallet connected !</h1>
     </div>
   );
 };
